cc968bbdce4fdcd7be85f442320d001bd08a74ae
nikonatra <nikonatra93@hotmail.com>
2018-11-29 20:29:09 -0500

    Creacion de las vistas

--- webApp/urls.py
+++ webApp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
      url(r'^resultado/$', views.resultado, name='resultado'),
+     url(r'^login/$', views.login, name='login'),
+     url(r'^index/$', views.index, name='index'),
+     url(r'^analistas/$', views.fanalistas, name='fanalistas'),
+     url(r'^entidades/$', views.fentidades, name='fentidades'),
 ]
--- webApp/views.py
+++ webApp/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-def formulario(request):
-	return render(request, 'webApp/formulario.html')
+def login(request):
+	return render(request, 'webApp/login.html')
+
+def index(request):
+	return render(request, 'webApp/index.html')
+
+def fanalistas(request):
+	return render(request, 'webApp/fanalistas.html')
+
+def fentidades(request):
+	return render(request, 'webApp/fentidades.html')	
 
 def resultado(request):
 	return render(request, 'webApp/resultado.html')
